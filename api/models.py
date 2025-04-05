c181a28c712d1e51a2d0ea71f4fb7a412f5ffe55
magnus.coder <kelimbth@gmail.com>
2025-04-05 13:28:07 +0300

    created Track model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    duration = models.FloatField()
+    last_play = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['artist', 'title']
