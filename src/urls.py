252cd22ff2fc62b04f258e23ec21b94d5d209a68
magnus.coder <kelimbth@gmail.com>
2025-04-06 11:51:04 +0300

    imported include function & added url for api app

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api.api import app
+
 
 urlpatterns = [
     path('api/', app.urls),
+    path('', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
